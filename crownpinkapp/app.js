de4fbdc56826e6cbee1876d6707882783eae9641
crownhackday <arun.degalaz@crownmelbourne.com.au>
2016-02-02 14:19:58 +1100

    Test

--- crownpinkapp/app.js
+++ crownpinkapp/app.js
@@ -1,16 +1,47 @@
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 var cfenv = require("cfenv");
 var appEnv = cfenv.getAppEnv();
 
-app.get('/', function (req, res) {
-  res.send('Day of the Week : ' + appEnv.url);
-});
+
 
 //app.listen(3000, function () {
  // console.log('Example app listening on port 3000!');
 //})
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+var d = new Date();
+var weekday = new Array(7);
+weekday[0]=  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ n });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
 
 app.listen(appEnv.port, appEnv.bind, function() {
     console.log("server starting on " + appEnv.url)
